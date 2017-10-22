d9324273ffbecf707ea3886c1a6a944066502fca
Jon Seaman <jonmseaman@live.com>
2017-10-21 22:07:47 -0400

    Added UnitTest file, and started redesigning code

--- GradingScript.py
+++ GradingScript.py
@@ -1,14 +1,19 @@
 # Author: Jon Seaman (.73)
-# Version: v1.0
-# Revised: 10/08/2017
+# Version: v1.1
+# Revised: 10/21/2017
 # Copyright (c) 2017 Jon Seaman
 
 import os
 from os.path import join
 
-if __name__ == "__main__":
+
+# TODO: Create a function that renames all of the projects by editing .project files.
+
+# TODO: Stop at the end of the program so that the user can see the output.
+
+def rename_projects(working_dir):
     # Allow the user to enter a directory to work from.
-    working_dir = input("Enter the path to the submissions directory: ")
+    original_dir = os.getcwd()
     os.chdir(working_dir)
     print("Working in: " + os.getcwd())
 
@@ -39,4 +44,30 @@ if __name__ == "__main__":
             print("Writing file..." + project_filename, end='')
             project_file = open(project_filename, mode='w')
             project_file.writelines(lines)
+            os.chdir(original_dir)
             print("\nDone.")
+
+
+def get_submissions_path():
+    path = input("Enter the path to the submission.zip: ")
+    while not os.path.isfile(path):
+        print("That is not a valid file path.")
+        path = input("Enter the path to the submission.zip: ")
+
+    return path
+
+
+def unzip_submission_file(path):
+    pass
+
+
+if __name__ == "__main__":
+    # Get the path to the submissions.zip
+    submissionsPath = get_submissions_path()
+    # Extract that file to a working directory.
+    unzip_submission_file(submissionsPath)
+
+    # Rename the projects that were extracted.
+    rename_projects(submissionsPath)
+
+    input("Press enter to exit...")
